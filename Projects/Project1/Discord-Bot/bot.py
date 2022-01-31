bc3515056303d398cdbd9d623a78e14fd4553a1a
Gerald Sellers <89589962+Gerselle@users.noreply.github.com>
2022-01-31 14:12:32 -0500

    Fixed commands

--- Projects/Project1/Discord-Bot/bot.py
+++ Projects/Project1/Discord-Bot/bot.py
@@ -1,11 +1,9 @@
 import os
-
 import discord
 import random
 from dotenv import load_dotenv
 
 load_dotenv()
-#print(os.getenv('DISCORD_TOKEN'))
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
@@ -51,19 +49,24 @@ async def on_message(message):
     # Bot responds with "Hello!" in 9 languages (ES, FR, JP, KR, RU, ZH-CN, TL, AR, DE)
     hey_there = ["¡Hola!", "Bonjour !", "こんにちは!", "여보세요!", "Привет!", "你好!", "Kamusta!", "مرحبا!", "Grüß Gott!"]
 
+    # Hitchhiker quote command
     if message.content == 'towel!':
-        #response = random.choice(brooklyn_99_quotes)
         response = random.choice(hitchhiker_quotes)
         await message.channel.send(response)
-    
+        
+    # Brooklyn 99 quote command
+    elif message.content == 'noice":
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+        
     # Greeting command
     elif message.content in greetings:
         response = random.choice(hey_there)
         await message.channel.send(response)
   
     # Picture command
-    if message.content == 'What do you have?':
-        image = random.choice("1234567") + ".jpg"
+    elif message.content == 'What do you have?':
+        image = random.choice("123456789") + ".jpg"
         await message.channel.send(file=discord.File(image))
 
 client.run(TOKEN)
